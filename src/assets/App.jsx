59cad0ad21143625ce8333ded10340b7d55e6487
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-10-12 14:21:19 +0100

    1 file modified

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,24 +1,10 @@
 import React from "react";
 import './App.css';
-import Wrapper from './components/Wrapper';
-import Screen from './components/Screen';
-import ButtonBox from './components/ButtonBox';
-import Button from './components/Button';
+
 
 function App() {
   return (
-    <Wrapper>
-      <Screen value="0" />
-      <ButtonBox>
-        <Button
-          className=""
-          value="0"
-          onClick={() => {
-            console.log('Button Clicked');
-          }}
-        />
-      </ButtonBox>
-    </Wrapper>
+  <div>Hi!</div>
   );
 }
 
